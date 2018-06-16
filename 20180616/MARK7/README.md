2fe104301e4f7f1814842178f02e1eb2f33851cd
Red <edgar.msilva@gmail.com>
2018-06-16 13:03:24 -0300

    aula de sábado 16/06/2018

---
+++ 20180616/MARK7/README.md
@@ -0,0 +1,14 @@
+
+
+# projeto mark7
+## gerenciadpr de tarefa
+
+# features 
+* Login
+* Cadastro de uruário
+* Cadastro de tarefas (CRUD) = Create, read, update, delete
+* Troca de senha
+* Minha conta (atualiazação cadastral)
+* Finalizar tarefa
+
+# Sprint 1 = Login
---
+++ 20180616/MARK7/features/step_definitions/login._steps.rb
@@ -0,0 +1,24 @@
+Dado("que eu acessei o formulário de login") do
+    visit 'https://mark7.herokuapp.com/login'
+  end
+  
+  Quando("faço o login com {string} e {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("sou autenticado com sucesso") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("sou redirecionado para o painel de tarefas com a mensagem {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("faço login com {string} e {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("devo ver a mensagem de alerta {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
