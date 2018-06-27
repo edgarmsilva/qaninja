079ff6f4aa6188e50434b69faf5ae5361f8e389c
Edgar <edgar.msilva@gmail.com>
2018-06-27 11:25:35 -0300

    correção do Caixa

--- Caixa/features/step_definitions/caixa_steps.rb
+++ Caixa/features/step_definitions/caixa_steps.rb
@@ -1,27 +1,20 @@
 require_relative '../../src/saque.rb'
 
-Dado("que eu tenho uma conta com {string} reais") do |inicial|
-    @inicial = inicial
+Dado("que eu tenho uma conta com {int} reais") do |saldo_inicial|
+    @conta = Conta.new(saldo_inicial)
   end
   
-  Quando("faço um saque no valor de {string} reais") do |saque_valor|
-    @saque = saque_valor
+  Quando("faço um saque no valor de {int} reais") do |saque_valor|
+    @resultado = @conta.saca(saque_valor)
     
   end
   
-  Então("{string} reais será meu saldo final") do |saldo_fim|
-    teste = Saque.new(@inicial,@saque_valor)
-    @saldo_final = teste.efetua_saque
-    # expect(saldo_final).to eql @resultado_saque.round(2)
+  Então("{int} reais será meu saldo final") do |saldo_fim|
+    expect(@conta.saldo).to eql saldo_fim
 
-    # puts saldo_fim
-    puts @saldo_final
   end
   
   Então("devo ver a seguinte mensagem: {string}") do |mensagem|
-    teste2 = Saque.new(@saldo_inicial,@saque_valor)
-    @msg = teste2.mostra_msg
-    # expect(mensagem).to eql @msg
-    puts mensagem
-    puts @msg
+    expect(@resultado).to eql mensagem
+
   end
--- Caixa/src/saque.rb
+++ Caixa/src/saque.rb
@@ -1,34 +1,19 @@
-class Saque
-    attr_accessor :saldo_inicial, :saque_valor 
+class Conta
+    attr_accessor :saldo
 
-    def initialize (saldo_inicial,saque_valor)
-        @saldo_inicial = saldo_inicial.to_f
-        @saque_valor = saque_valor.to_f
+    def initialize(saldo)
+        @saldo = saldo
+    end
+
+    def saca(valor)
+        if valor > @saldo
+            return "Saldo insuficiente para saque."
+           
+        else
+            @saldo -= valor
+            return "Saque com sucesso. Muito Obrigado!"
+
+        end
         
     end
-
-    def efetua_saque
-        if @saldo_inicial > @saque_valor
-            return saldo_inicial - saque_valor
-            
-        else
-            return "deu ruim"
-        end
-    end
-
-    def mostra_msg
-        if saldo_inicial > saque_valor
-            return "Saque com sucesso. Muito Obrigado!"
-        else
-            
-            return "Saldo insuficiente para saque."
-        end
-    end
-    
 end
-
-
-
-
-
-
