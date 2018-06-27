0d78243699dcd6da8bd69fbd44818a462024593e
Edgar <edgar.msilva@gmail.com>
2018-06-27 11:52:25 -0300

    aaa

--- Caixa/src/saque.rb
+++ Caixa/src/saque.rb
@@ -1,7 +1,6 @@
 class Conta
     attr_accessor :saldo
 
-<<<<<<< HEAD
     def initialize(saldo)
         @saldo = saldo
     end
@@ -12,26 +11,6 @@ class Conta
            
         else
             @saldo -= valor
-=======
-    # def initialize (saldo_inicial,saque_valor)
-    #     @saldo_inicial = saldo_inicial.to_f
-    #     @saque_valor = saque_valor.to_f
-        
-    # end
-
-    def efetua_saque(saldo_inicial,saque_valor)
-        if (saldo_inicial > saque_valor)
-            res = saldo_inicial - saque_valor
-            return res
-            
-        else
-            return saldo_inicial
-        end
-    end
-
-    def mostra_msg(saldo_inicial,saque_valor)
-        if saldo_inicial > saque_valor
->>>>>>> cd7b85c632730a8511e7be676a141ce0bfc8facb
             return "Saque com sucesso. Muito Obrigado!"
 
         end
