47ea3b44ef3ec189df8d3e5505598e96505433fc
Edgar <edgar.msilva@gmail.com>
2018-06-27 12:18:28 -0300

    ok

--- Caixa/src/saque.rb
+++ Caixa/src/saque.rb
@@ -8,12 +8,9 @@ class Conta
     def saca(valor)
         if valor > @saldo
             return "Saldo insuficiente para saque."
-           
         else
             @saldo -= valor
             return "Saque com sucesso. Muito Obrigado!"
-
         end
-        
     end
 end
